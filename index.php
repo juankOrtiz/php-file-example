6785930342f1a444f9d9b94b9d8bd83b8cd55a3f
juankOrtiz <juank.ortiz.1991@gmail.com>
2024-01-25 17:42:16 -0300

    Creación del proyecto

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require_once __DIR__ . '/inc/flash.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css" />
+    <title>PHP File Upload</title>
+</head>
+
+<body>
+
+    <?php flash('upload') ?>
+
+    <main>
+        <form enctype="multipart/form-data" action="upload.php" method="post">
+            <div>
+                <label for="file">Select a file:</label>
+                <input type="file" id="file" name="file" />
+            </div>
+            <div>
+                <button type="submit">Upload</button>
+            </div>
+        </form>
+    </main>
+</body>
+
+</html>
